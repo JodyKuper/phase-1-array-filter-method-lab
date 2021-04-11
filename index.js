616ab937f529a65558bbb493132a4fea6537e4ec
Joel Cooper <jodycooper16@gmail.com>
2021-04-11 14:33:51 -0700

    commit tree c4cdc88873ad4ca670326cdf5c29908cc9d25c29

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution here
+
+const findMatching = (drivers, searchName)=>{
+ const theMatching = drivers.filter((driver) => {
+    return driver.toLowerCase() === searchName.toLowerCase()
+})
+    return theMatching;
+
+}
+
+const fuzzyMatch = (drivers,letters)=>{
+    const aMatch = drivers.filter ((driver) => {
+        const sliceName = driver.slice(0,2).toLowerCase()
+        return sliceName === letters.toLowerCase()  
+    })
+
+    return aMatch;
+
+}
+const matchName = (drivers, searchName) =>{
+    const foundsNames = drivers.filter((driver) => {
+        return driver.name === searchName;
+    })
+    return foundsNames;
+}
+
+// const findBobby = findMatching('Bobby', drivers,)
